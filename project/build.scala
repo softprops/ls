5cdf85abb4f93b9a17c17c31b93af8a4ce1ee41c
softprops <d.tangren@gmail.com>
2011-11-02 21:34:44 -0400

    split the shim

--- project/build.scala
+++ project/build.scala
@@ -7,77 +7,6 @@ object Build extends sbt.Build {
   import less.Plugin._
   //import heroic.Plugin._
 
-
-  object HeroShim {
-    import java.io.File
-
-    object Script {
-      def apply(main: String, cp: Seq[String], jvmOpts: Seq[String]) =
-        """#!/bin/sh
-        |
-        |CLEAR="\033[0m"
-        |
-        |info (){
-        |  COLOR="\033[0;35m"
-        |  echo "$COLOR $1 $CLEAR"
-        |}
-        |
-        |error (){
-        |  COLOR="\033[0;31m"
-        |  echo "$COLOR $1 $CLEAR"
-        |}
-        |
-        |JAVA=`which java`
-        |
-        |CLASSPATH=%s
-        |
-        |info "Booting application (%s)"
-        |exec $JAVA %s -classpath "$CLASSPATH" %s "$@"
-        |""".stripMargin
-         .format(
-          cp.mkString(":"),
-          main,
-          jvmOpts.mkString(" "),
-          main
-        )
-    }
-
-    private def rootDir(state: State) =
-      file(Project.extract(state).structure.root.toURL.getFile)
-
-    private def relativeToRoot(state: State, f: File) =
-      IO.relativize(rootDir(state), f)
-
-    lazy val stage = TaskKey[Unit]("stage", "heroku hook")
-
-    def shimNoopSettings: Seq[Setting[_]] = Seq(
-      stage <<= (streams) map {
-        (out) => out.log.info("noop")
-      }
-    )
-
-    def shimSettings: Seq[Setting[_]] = Seq(
-      stage <<= (mainClass in Compile, streams, fullClasspath in Runtime, state,
-       target, javaOptions in Compile) map {
-        (main, out, cp, state, target, jvmOpts) => main match {
-          case Some(mainCls) =>
-            val scriptBody = HeroShim.Script(mainCls, cp.files map { f =>
-              relativeToRoot(state, f) match {
-                case Some(rel) => rel
-                case _ => f.getAbsolutePath
-              }
-            }, jvmOpts)
-            val sf = new File(target, "hero")
-            out.log.info("Writing hero file, %s" format sf)
-            IO.write(sf, scriptBody)
-            sf.setExecutable(true)
-            sf
-          case _ => sys.error("Main class required")
-        }
-      }
-    )
-  }
-
   object Resolvers {
     val coda = "coda" at "http://repo.codahale.com"
   }
@@ -87,13 +16,13 @@ object Build extends sbt.Build {
     version := "0.1.0-SNAPSHOT"
   )
 
-  lazy val root = Project("ls", file("."), settings = buildSettings ++ Seq(HeroShim.stage in Compile := {})) aggregate(
+  lazy val root = Project("ls", file("."), settings = buildSettings ++ Seq(
+    HeroShim.stage in Compile := {})) aggregate(
     svr, plugin
   )
 
   lazy val lib = Project("library", file("library"),
                        settings = buildSettings ++ Seq(name := "ls"))
-
 
   lazy val svr = Project("server", file("server"),
                          settings = buildSettings ++ Seq(
