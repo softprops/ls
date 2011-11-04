33c054fdf4463a1f089a227c79b5b8e1419add83
softprops <d.tangren@gmail.com>
2011-11-03 21:37:59 -0400

    page tracking

--- server/src/main/scala/templates.scala
+++ server/src/main/scala/templates.scala
@@ -160,6 +160,16 @@ object Templates {
       <link rel="stylesheet" type="text/css" href="/css/ls.css" />
       { sheets.map { s => <link rel="stylesheet" type="text/css" href="/css/%s.css" /> } }
       <script type="text/javascript" src="/js/jquery.min.js"></script>
+      <script type="text/javascript"><![CDATA[
+       var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-26788051-1']);
+        _gaq.push(['_trackPageview']);
+       (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+       })();]]>
+    </script>
     </head>
      <body>
        <div id="container">
