57cf199dfe3af55bd364bc670c5cb6313332c1bb
Nathan Hamblen <nathan@technically.us>
2011-11-05 20:20:19 -0400

    append to build

--- app/src/main/scala/init.scala
+++ app/src/main/scala/init.scala
@@ -1,21 +1,23 @@
 package ls
 
+import java.io.{File,FileWriter}
+
 object LsInit {
   def main(args: Array[String]) {
     val exit = run(args)
     System.exit(exit)
   }
   def run(args: Array[String]): Int = {
-    val base = new java.io.File(args.headOption.getOrElse("."))
+    val base = new File(args.headOption.getOrElse("."))
     val result = if (base.isDirectory) {
       lsVersion match {
         case Some(version) =>
-          Right(version)
+          setup(base, version)
         case None =>
           Left("Unable to retrive current ls version")
       }
     } else {
-      Left("Directory not found: " + base.getAbsolutePath)
+      Left("Directory not found: " + base.getCanonicalPath)
     }
     result.fold(
       { err => println(err); 1 },
@@ -29,7 +31,7 @@ object LsInit {
     try {
       http(req >- { js =>
         scala.util.parsing.json.JSON.parseFull(js).map {
-          _.asInstanceOf[Map[String,Any]]("version")
+          _.asInstanceOf[Map[String,Any]]("version").toString
         }
       })
     } catch {
@@ -37,6 +39,45 @@ object LsInit {
     } finally {
       http.shutdown()
     }
+  }
+  def setup(base: File, version: String) = {
+    val build = new File(base, "build.sbt")
+    val plugin = new File(base, "project/plugins.sbt")
+    val files = build :: plugin :: Nil
+    def stat(f: File) =
+      "%s: %s".format(
+        if (f.exists) "Updated" else "Created",
+        f.getCanonicalPath
+      )
+    val msg = files.map(stat).mkString("\n")
+    files.foreach { f =>
+      new File(f.getParent).mkdirs()
+    }
+    def write(f: File)(block: FileWriter => Unit) {
+      val writer = new FileWriter(f, true)
+      try {
+        block(writer)
+      } finally {
+        writer.close()
+      }
+    }
+    write(build) { fw =>
+      fw.write("""
+      |
+      |seq(lsSettings :_*)
+      |""".stripMargin)
+    }
+    write(plugin) { fw =>
+      fw.write("""
+      |
+      |resolvers ++= Seq(
+      |  "less is" at "http://repo.lessis.me",
+      |  "coda" at "http://repo.codahale.com")
+      |
+      |addSbtPlugin("me.lessis" % "ls-sbt" % "0.1.0-RC1")
+      |""".stripMargin)
+    }
+    Right(msg)
   }
 }
 class LsInit extends xsbti.AppMain {
