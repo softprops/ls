62cde166504c1929b52152cc0a1f0210afdc9ed1
Nathan Hamblen <nathan@technically.us>
2011-11-25 01:30:36 -0500

    actually use the looked up version

--- app/src/main/scala/init.scala
+++ app/src/main/scala/init.scala
@@ -74,8 +74,8 @@ object LsInit {
       |  "less is" at "http://repo.lessis.me",
       |  "coda" at "http://repo.codahale.com")
       |
-      |addSbtPlugin("me.lessis" % "ls-sbt" % "0.1.0-RC1")
-      |""".stripMargin)
+      |addSbtPlugin("me.lessis" % "ls-sbt" % "%VERSION%")
+      |""".replace("%VERSION%", version).stripMargin)
     }
     Right(msg)
   }
