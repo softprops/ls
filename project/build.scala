d22b5c22d523bf997e9775b3f9a5511294741044
softprops <d.tangren@gmail.com>
2011-12-01 00:47:12 -0500

    toward 0.1.1

--- project/build.scala
+++ project/build.scala
@@ -13,7 +13,7 @@ object Build extends sbt.Build {
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "me.lessis",
-    version := "0.1.1-SNAPSHOT",
+    version := "0.1.1",
     publishTo :=  Some(Resolver.file("lessis repo", new java.io.File("/var/www/repo")))
   )
   val dispatchVersion = "0.8.6"
