67552515dbeaca6c1f37cdf57de37fcecaeeb888
softprops <d.tangren@gmail.com>
2012-02-15 23:08:09 -0500

    add organization attr to Version type

--- library/src/main/scala/types.scala
+++ library/src/main/scala/types.scala
@@ -5,6 +5,7 @@ case class Dependencies(libraries: Seq[ModuleID])
 
 case class User(login: String, id: Int, avatar_url: String, url: String)
 case class License(name: String, url: String)
+
 // api request representation
 case class Library(organization: String, name: String, version: String,
                    description: String, site: String, tags: Seq[String],
@@ -17,18 +18,22 @@ case class Library(organization: String, name: String, version: String,
                    contributors: Option[Seq[User]] = None)
 
 // internal representation
-case class Version(version: String, docs: String,
-                   resolvers: Seq[String],
-                   dependencies: Seq[ModuleID],
-                   scalas: Seq[String],
-                   licenses: Seq[License])
+case class Version(
+  version: String, docs: String,
+  resolvers: Seq[String],
+  dependencies: Seq[ModuleID],
+  scalas: Seq[String],
+  licenses: Seq[License],
+  organization: String)// new since 0.1.2 the new preferred way to persist origanization info
 
-case class LibraryVersions(organization: String, name: String,
-                           description: String, site: String, tags: Seq[String],
-                           updated: Long,
-                           versions: Seq[Version],
-                           sbt: Boolean = true,
-                           ghuser: Option[String] = None, ghrepo: Option[String] = None,
-                           contributors: Option[Seq[User]] = None)
+case class LibraryVersions(
+  organization: String, // deprecated in favor of allowing versions to declare organizations
+  name: String,
+  description: String, site: String, tags: Seq[String],
+  updated: Long,
+  versions: Seq[Version],
+  sbt: Boolean = true,
+  ghuser: Option[String] = None, ghrepo: Option[String] = None,
+  contributors: Option[Seq[User]] = None)
 
 
