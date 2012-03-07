7bb972c0deca56f8cac66a08907e4243261398fe
softprops <d.tangren@gmail.com>
2012-03-07 01:39:16 -0500

    try getting the stage set for a repub of 0.1.1 in a world without scala-tools

--- project/build.scala
+++ project/build.scala
@@ -2,9 +2,9 @@ import sbt._
 import Keys._
 
 object Build extends sbt.Build {
-  import coffeescript.Plugin._
-  import less.Plugin._
-  import ls.Plugin._
+  //import coffeescript.Plugin._
+  //import less.Plugin._
+  //import ls.Plugin._
   //import heroic.Plugin._
 
   object Resolvers {
@@ -14,76 +14,72 @@ object Build extends sbt.Build {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "me.lessis",
     version := "0.1.1",
-    publishTo :=  Some(Resolver.file("lessis repo", new java.io.File("/var/www/repo")))
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) 
+        Some("snapshots" at nexus + "content/repositories/snapshots") 
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    homepage :=
+      Some(new java.net.URL("http://ls.implicit.ly/")),
+    publishArtifact in Test := false,
+    licenses <<= (version)(v => Seq("MIT" -> url("https://github.com/softprops/tree/master/%s/LICENSE".format(v)))),
+    pomExtra := (
+      <scm>
+        <url>git@github.com:softprops/ls.git</url>
+        <connection>scm:git:git@github.com:softprops/ls.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>softprops</id>
+          <name>Doug Tangren</name>
+          <url>https://github.com/softprops</url>
+        </developer>
+      </developers>)
   )
   val dispatchVersion = "0.8.6"
 
   lazy val root = Project("root", file("."), settings = buildSettings ++ Seq(
     HeroShim.stage in Compile := {})) aggregate(
-    svr, plugin, lib, app
+    plugin, lib, app
   ) 
 
   lazy val lib = Project("library", file("library"),
-    settings = buildSettings ++ Seq(name := "ls"))
-
-  lazy val svr = Project("server", file("server"),
-    settings = buildSettings ++ Seq(
-      name := "ls-server",
-      scalacOptions += "-deprecation",
-      libraryDependencies ++= Seq(
-        "com.codahale" %% "jerkson" % "0.5.0",
-        "net.databinder" %% "dispatch-http" % dispatchVersion,
-        "net.databinder" %% "unfiltered-netty-server" % "0.5.1",
-        "com.mongodb.casbah" %% "casbah" % "2.1.5-1"
-      )) ++ coffeeSettings ++ lessSettings ++ HeroShim.shimSettings ++ /* heroicSettings ++ */
-         Seq(
-           (resourceManaged in (Compile, CoffeeKeys.coffee)) <<= (resourceManaged in Compile) {
-             _ / "www" / "js"
-           },
-           (resourceManaged in (Compile, LessKeys.less)) <<= (resourceManaged in Compile) {
-             _ / "www" / "css"
-           },
-           (LessKeys.mini in (Compile, LessKeys.less)) := true,
-           resolvers += Resolvers.coda
-         )) dependsOn(lib)
+    settings = buildSettings ++ Seq(name := "ls", description := "ls library"))
 
   lazy val plugin = Project("plugin", file("plugin"),
     settings = buildSettings ++ Seq(
       sbtPlugin := true,
       name := "ls-sbt",
+      description := "An sbt interface for ls.implicit.ly",
       libraryDependencies ++= Seq(
         "com.codahale" %% "jerkson" % "0.5.0",
         "net.databinder" %% "dispatch-http" % dispatchVersion
       ),
-      resolvers += Resolvers.coda
-    ) ++ ScriptedPlugin.scriptedSettings ++ lsSettings ++ Seq(
-      description in LsKeys.lsync := "An sbt interface for ls.implicit.ly",
+      resolvers += Resolvers.coda,
+      publishMavenStyle := false,
+      publishTo := Some(Resolver.url("sbt-plugin-releases", url(
+        "http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"
+      ))(Resolver.ivyStylePatterns))
+    ) ++ ScriptedPlugin.scriptedSettings /**++ lsSettings ++ Seq(      
       LsKeys.tags in LsKeys.lsync := Seq("ls", "plugin", "sbt"),
       externalResolvers in LsKeys.lsync := Seq(
-        "less is" at "http://repo.lessis.me",
         "coda" at "http://repo.codahale.com"
       ),
-      LsKeys.docsUrl in LsKeys.lsync := Some(url("http://ls.implicit.ly/#publishing")),
-      homepage in LsKeys.lsync := Some(url("http://ls.implicit.ly/")),
-      licenses in LsKeys.lsync := Seq(
-        ("MIT", url("https://github.com/softprops/ls/blob/master/LICENSE"))
-      )
-    )) dependsOn(lib) /* here will not work in sbt as it generates pom with a sources classifier,
+      LsKeys.docsUrl in LsKeys.lsync := Some(url("http://ls.implicit.ly/#publishing"))
+    )*/) dependsOn(lib) /* here will not work in sbt as it generates pom with a sources classifier,
       references to class with in at that point result in class not found exceptions. sbt xsbt issue #257 */
 
   lazy val app = Project("app", file("app"),
     settings = buildSettings ++ 
       conscript.Harness.conscriptSettings ++ Seq(
         name := "ls-app",
+        description := "ls project intializer",
         libraryDependencies += 
         "net.databinder" %% "dispatch-http" % dispatchVersion
       )
     ) dependsOn(lib)
-
-  /*lazy val tools = Project("ls-tools", file("tools"),
-      settings = buildSettings ++ Seq(
-        libraryDependencies += "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
-        sbtPlugin := true
-      )) dependsOn(lib)*/
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,13 +1,13 @@
 resolvers ++= Seq(
-  "lessis" at "http://repo.lessis.me",
+//  "lessis" at "http://repo.lessis.me",
   "coda" at "http://repo.codahale.com"
 )
 
-addSbtPlugin("me.lessis" % "coffeescripted-sbt" % "0.2.0")
+//addSbtPlugin("me.lessis" % "coffeescripted-sbt" % "0.2.0")
 
-addSbtPlugin("me.lessis" % "less-sbt" % "0.1.3")
+//addSbtPlugin("me.lessis" % "less-sbt" % "0.1.3")
 
-addSbtPlugin("me.lessis" % "ls-sbt" % "0.1.1")
+//addSbtPlugin("me.lessis" % "ls-sbt" % "0.1.1")
 
 //addSbtPlugin("me.lessis" % "heroic" % "0.1.0-SNAPSHOT")
 
