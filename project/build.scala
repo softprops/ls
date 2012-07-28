164f00fc8c6176310ea8baf7abc8547dacd2108f
softprops <d.tangren@gmail.com>
2012-07-28 16:16:17 -0400

    should work

--- project/build.scala
+++ project/build.scala
@@ -4,8 +4,6 @@ import Keys._
 object Build extends sbt.Build {
   import coffeescript.Plugin._
   import less.Plugin._
-  import ls.Plugin._
-  //import heroic.Plugin._
 
   object Resolvers {
     val coda = "coda" at "http://repo.codahale.com"
@@ -13,14 +11,14 @@ object Build extends sbt.Build {
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "me.lessis",
-    version := "0.1.1",
+    version := "0.1.2",
     publishTo :=  Some(Resolver.file("lessis repo", new java.io.File("/var/www/repo")))
   )
   val dispatchVersion = "0.8.6"
 
   lazy val root = Project("root", file("."), settings = buildSettings ++ Seq(
     HeroShim.stage in Compile := {})) aggregate(
-    svr, plugin, lib, app
+    plugin, lib
   ) 
 
   lazy val lib = Project("library", file("library"),
@@ -56,29 +54,13 @@ object Build extends sbt.Build {
         "net.databinder" %% "dispatch-http" % dispatchVersion
       ),
       resolvers += Resolvers.coda
-    ) ++ ScriptedPlugin.scriptedSettings ++ lsSettings ++ Seq(
-      description in LsKeys.lsync := "An sbt interface for ls.implicit.ly",
-      LsKeys.tags in LsKeys.lsync := Seq("ls", "plugin", "sbt"),
-      externalResolvers in LsKeys.lsync := Seq(
-        "less is" at "http://repo.lessis.me",
-        "coda" at "http://repo.codahale.com"
-      ),
-      LsKeys.docsUrl in LsKeys.lsync := Some(url("http://ls.implicit.ly/#publishing")),
-      homepage in LsKeys.lsync := Some(url("http://ls.implicit.ly/")),
-      licenses in LsKeys.lsync := Seq(
+    ) ++ ScriptedPlugin.scriptedSettings ++  Seq(
+      description  := "An sbt interface for ls.implicit.ly",
+      homepage := Some(url("http://ls.implicit.ly/")),
+      licenses := Seq(
         ("MIT", url("https://github.com/softprops/ls/blob/master/LICENSE"))
       )
-    )) dependsOn(lib) /* here will not work in sbt as it generates pom with a sources classifier,
-      references to class with in at that point result in class not found exceptions. sbt xsbt issue #257 */
-
-  lazy val app = Project("app", file("app"),
-    settings = buildSettings ++ 
-      conscript.Harness.conscriptSettings ++ Seq(
-        name := "ls-app",
-        libraryDependencies += 
-        "net.databinder" %% "dispatch-http" % dispatchVersion
-      )
-    ) dependsOn(lib)
+    )) dependsOn(lib)
 
   /*lazy val tools = Project("ls-tools", file("tools"),
       settings = buildSettings ++ Seq(
